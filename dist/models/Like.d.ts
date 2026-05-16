b344b4b5c66c91b1c884adcd4c99b07962d5bb53
abhayaos <nepalcodinghouse@gmail.com>
2026-05-16 22:07:08 +0545

    add common js

--- dist/models/Like.d.ts
+++
@@ -1,8 +0,0 @@
-import mongoose, { Document } from "mongoose";
-export interface ILike extends Document {
-    confessionId: mongoose.Types.ObjectId;
-    anonymousId: string;
-    createdAt: Date;
-}
-export declare const Like: mongoose.Model<any, {}, {}, {}, any, any, any>;
-//# sourceMappingURL=Like.d.ts.map
--- dist/models/Like.js
+++
@@ -1,8 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-const LikeSchema = new Schema({
-    confessionId: { type: Schema.Types.ObjectId, ref: "Confession", required: true },
-    anonymousId: { type: String, required: true },
-}, { timestamps: true });
-LikeSchema.index({ confessionId: 1, anonymousId: 1 }, { unique: true });
-export const Like = mongoose.models.Like || mongoose.model("Like", LikeSchema);
-//# sourceMappingURL=Like.js.map
